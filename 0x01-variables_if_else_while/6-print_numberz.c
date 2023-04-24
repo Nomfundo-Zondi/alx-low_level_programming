708407385e22e222190b0551e7e37b6c1ef2392b
Nomfundo <Nomfundozondi18@gmail.com>
2023-04-24 10:46:36 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print numbers starting from 0
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+  int b;
+
+  for (b = '0'; b <= '9'; d++)
+    putchar(b);
+  putchar('\n');
+
+  return (0);
+}
