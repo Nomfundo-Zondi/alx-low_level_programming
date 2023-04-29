cc096bce5ac763a621788d3f32dcf35b08ff5b98
Nomfundo <Nomfundozondi18@gmail.com>
2023-04-28 20:56:49 -0700

    print fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the numbers from 1 to 100
+ * 3 multiplies print fizz instead of the number
+ * 5 multiplies print fizz instead of the number
+ * 3 & 5 multiplies print Fizzbuzz instead of number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char
