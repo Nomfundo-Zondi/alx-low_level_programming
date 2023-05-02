34b6e3a155fa804b9e5818c875243664cc66d42e
Nomfundo <Nomfundozondi18@gmail.com>
2023-05-02 11:04:37 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * * main - check code
+ * Return: Always 0. Success.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computs. I fear the lack of them - Isacc Asimov";
+	print_rev(str);
+	return (0);
+}
