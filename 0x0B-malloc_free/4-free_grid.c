8c3b5e9e1f7d3dc0c8709cc9166c3b1ff513b251
Nomfundo <Nomfundozondi18@gmail.com>
2023-05-16 05:37:23 -0700

    4-free_gric

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free grid - function that frees a 2D array
+ * free_grid - function that frees a 2D array
  * @grid: input 2D array of integers
  * @height: grid height
  * Return: Nothing
