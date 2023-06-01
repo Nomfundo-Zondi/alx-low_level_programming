e88e3815733641addac7c6d59880b115ce7ac489
Nomfundo <Nomfundozondi18@gmail.com>
2023-06-01 10:20:44 -0700

    100-first.c print function first before main code

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - print function first before main function
+ *
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're the beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
+
