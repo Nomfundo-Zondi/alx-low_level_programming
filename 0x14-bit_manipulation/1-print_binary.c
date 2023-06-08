6b0593a710e57a47871b8797475615a234a2fd46
Nomfundo <Nomfundozondi18@gmail.com>
2023-06-08 10:13:36 -0700

    1-print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number representing binary
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	_divide(n);
+}
+
+/**
+ * _divide - division of binary representations
+ * @n: number representing binary
+ *
+ * Return: binary
+ */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
+
